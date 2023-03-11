50d7cfdfc09ccbf149ce0711fd42ce3c032e5538
Priestch <priestch@gmail.com>
2023-03-11 14:32:25 +0800

    fix: fix crate structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod kugou;
+pub mod kuwo;
+pub mod media;
+pub mod migu;
+pub mod netease;
+pub mod qq;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod utils;
